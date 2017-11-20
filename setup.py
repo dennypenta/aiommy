b3c15d811adf48e517b70b85e69abac17b3ccf94
Denis Dvornikov <candyboobers@gmail.com>
2017-11-20 11:39:25 +0300

    0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='aiommy',
-    version='0.0.2',
+    version='0.0.3',
 
     description='A helpful tools for building web API',
     long_description=long_description,
